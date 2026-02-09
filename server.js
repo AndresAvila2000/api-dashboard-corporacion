5fb0fa8d3271efeeea27467f5c0dabb6cbd15d70
AndresAvila2000 <163436978+AndresAvila2000@users.noreply.github.com>
2026-02-09 09:39:37 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const HOST = const HOST = '0.0.0.0';
+const HOST = '0.0.0.0';
 
 // Middleware
 app.use(cors());
